d618f954882f42f74234ab2443ea33cc5ed3e838
garethduncandev <garethduncandev@outlook.com>
2023-10-02 19:34:42 +0100

    base url

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import postcssNesting from 'postcss-nesting';
 
 export default defineConfig({
+  base: '/code.wales/',
   css: {
     postcss: {
       plugins: [postcssNesting],
